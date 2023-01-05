7b650fb69b7cd4cb341fa5f88f2e2df5d5185ee7
tricky <trickya23@outlook.com>
2023-01-05 05:47:27 +0000

    last digit

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+def to_upper(chr):
+    if ord(chr) >= 97 and ord(chr) <= 122:
+        return (ord(chr) - 32)
+
+
 def uppercase(str):
-    for i in str:
-        if ord(i) >= 97 and ord(i) <= 122:
-            print(chr(ord(i) - 32), end="")
-        else:
-            print(i, end="")
-    print()
+    new_string = ""
+    for chr in str:
+        new_string += "%c" % to_upper(chr)
+
+    print("{:s}" .format(new_string))
