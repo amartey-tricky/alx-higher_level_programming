8ed924d4f76cd10934cc9003ccd302d6b906e685
tricky <trickya23@outlook.com>
2023-01-08 14:28:53 +0000

    calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
+
+    operators = {
+        "+": add,
+        "-": sub,
+        "*": mul,
+        "/": div
+    }
+
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+    operator = argv[2]
+
+    if operator not in operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    print("{} {} {} = {}".format(a, operator, b, operators[operator](a, b)))
