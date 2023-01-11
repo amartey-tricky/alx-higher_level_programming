8bb82a171e70c4e7bc7382432606a6f14d6470ed
tricky <trickya23@outlook.com>
2023-01-11 09:06:55 +0000

    tuple

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for x in matrix:
-        for y in range(len(x)):
-            if y == len(x) - 1:
-                print("{:d}".format(x[y]), end="")
-            else:
-                print("{:d}".format(x[y]), end=" ")
+    for row in matrix:
+        print(" ".join("{:d}".format(index) for index in row))
